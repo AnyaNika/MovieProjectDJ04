226027f934b42efa17a2e7b18bdcf3e97af5c3f6
Anna <nikoshina.anna@gmail.com>
2025-07-02 16:52:27 +0300

    finish 1

--- movie_project/films/forms.py
+++ movie_project/films/forms.py
@@ -5,3 +5,8 @@ class FilmForm(forms.ModelForm):
     class Meta:
         model = Film
         fields = ['title', 'description', 'review']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Название фильма'}),
+            'description': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Описание фильма'}),
+            'review': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Отзыв'})
+        }
